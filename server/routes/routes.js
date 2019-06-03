6a57b2c1853e7f0f3b01ff6834f8f4fb611c5f09
Rasmus Klausen <166102@rts-365.dk>
2019-06-03 13:00:46 +0200

    03-06-19 13.00

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,23 @@
-module.exports = (app) => {
 
-   app.get('/', (req, res, next) => {
-      res.render('home');
-   });
+   module.exports = (app) => {
 
-};
+      app.get('/', (req, res, next) => {
+         res.render('home');
+      });
+   
+      app.get('/about', (req, res, next) => {
+         res.render('about');
+      });
+   
+      app.get('/contact', (req, res, next) => {
+         res.render('contact');
+      });
+   
+      app.get('/categories-post', (req, res, next) => {
+         res.render('categories-post');
+      });
+   
+      app.get('/single-post', (req, res, next) => {
+         res.render('single-post');
+      });
+   };
